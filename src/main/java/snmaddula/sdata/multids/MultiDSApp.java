b50e92fe967a323d119d4a25466d65a44cbdcb11
snmaddula <sn_maddula@outlook.com>
2018-09-23 23:37:43 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/sdata/multids/MultiDSApp.java
@@ -0,0 +1,17 @@
+package snmaddula.sdata.multids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author snmaddula
+ *
+ */
+@SpringBootApplication
+public class MultiDSApp {
+
+	public static void main(String...args) {
+		SpringApplication.run(MultiDSApp.class, args);
+	}
+}
